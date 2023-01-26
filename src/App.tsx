9b9debe66af7d1de43a895e7aa3d86aa86cc1931
harshcut <harshskarande@gmail.com>
2023-01-26 14:51:29 +0530

    update for multiple users

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,43 @@ import * as React from 'react'
 import { supabase } from '../utils'
 import { Cursor, MousePosition } from './components'
 
+interface User {
+  user_id: string
+  mouse_position: MousePosition
+}
+
+interface Payload<T> {
+  type: string
+  payload?: T
+  event: string
+}
+
+const userId = crypto.randomUUID()
+
 function App() {
-  const [mousePosition, setMousePosition] = React.useState<MousePosition>({ x: null, y: null })
+  const [users, setUsers] = React.useState<Record<string, User>>({})
 
   React.useEffect(() => {
     // create a channel with a unique name
     const channel = supabase.channel('room-name')
 
     const mouseEventHandler = (ev: MouseEvent) => {
-      // broadcast client cursor position to the channel
+      // broadcast client cursor position to the channel with user id
       channel.send({
         type: 'broadcast',
-        payload: { x: ev.clientX, y: ev.clientY },
+        payload: {
+          user_id: userId,
+          mouse_position: { x: ev.clientX, y: ev.clientY },
+        },
         event: 'cursor-pos',
       })
     }
 
     // listen for messages from the server
-    channel.on('broadcast', { event: 'cursor-pos' }, ({ payload }) => {
-      setMousePosition(payload)
+    channel.on('broadcast', { event: 'cursor-pos' }, ({ payload }: Payload<User>) => {
+      if (!payload) return
+      const { user_id, mouse_position } = payload
+      setUsers((prev) => ({ ...prev, [user_id]: { user_id, mouse_position } }))
     })
 
     // subscribe registers your client with the server
@@ -38,7 +56,11 @@ function App() {
 
   return (
     <>
-      <Cursor mousePosition={mousePosition} />
+      <span>{userId}</span>
+      {Object.entries(users).reduce((acc, [_, { mouse_position, user_id }]) => {
+        acc.push(<Cursor key={user_id} mousePosition={mouse_position} />)
+        return acc
+      }, [] as React.ReactElement[])}
     </>
   )
 }
